0a6bc1f0f749b860ac1bf9e8a71ee136c12b46e9
jusidama18 <jusidama18@gmail.com>
2022-02-21 15:09:40 +0700

    Update

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -3,3 +3,4 @@ from .ikb import keyboard, ikb
 from .errors import capture_error
 from .formatter import get_readable_file_size, get_readable_time, is_supported, new_thread
 from .parser import gdtot, appdrive
+from .database import *
