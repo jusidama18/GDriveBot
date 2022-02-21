aed81aed40fd76f3f2027edf683bf4b7a99b4d4a
jusidama18 <jusidama18@gmail.com>
2022-02-21 15:08:22 +0700

    Update

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,13 +3,28 @@ from asyncio import get_event_loop, sleep, exceptions
 from pyrogram import idle
 
 from bot.modules import ALL_MODULES
-from bot import app
+from bot.utils.database import clean_restart
+from bot import app, LOG_CHAT
 
 for module in ALL_MODULES:
     imported_module = import_module(f'bot.modules.{module}')
     reloads(imported_module)
 
-async def start_bot(): # Will add things later
+async def start_bot():
+    data = await clean_restart()
+    try:
+        if data:
+            await app.edit_message_text(
+                data["chat_id"],
+                data["message_id"],
+                "**Restarted Successfully**",
+            )
+
+        else:
+            await app.send_message(LOG_CHAT, "Bot started!")
+    except Exception:
+        pass
+
     await idle()
     await app.stop()
 
--- bot/utils/database.py
+++ bot/utils/database.py
@@ -3,5 +3,28 @@ from bot import db
 authdb = db.auths
 folderdb = db.folders
 searchdb = db.search
-
+restartdb = db.restart
 # Later
+
+async def start_restart(chat_id: int, message_id: int):
+    await restartdb.update_one(
+        {"something": "something"},
+        {
+            "$set": {
+                "chat_id": chat_id,
+                "message_id": message_id,
+            }
+        },
+        upsert=True,
+    )
+
+
+async def clean_restart() -> dict:
+    data = await restartdb.find_one({"something": "something"})
+    if not data:
+        return {}
+    await restartdb.delete_one({"something": "something"})
+    return {
+        "chat_id": data["chat_id"],
+        "message_id": data["message_id"],
+    }
